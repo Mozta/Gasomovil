d5a82aa3e89a4bde71d6b3d697866feac95c83d3
Mozta <viper_rafa_@hotmail.com>
2017-11-14 23:25:53 -0600

    Tab faqs for Activity help with library of expandable layout

--- app/src/main/java/com/idit/gasomovil/menu/MenuHelpActivity.java
+++ app/src/main/java/com/idit/gasomovil/menu/MenuHelpActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,12 +13,21 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.idit.gasomovil.Adapter.MyAdapter;
+import com.idit.gasomovil.Model.Item;
 import com.idit.gasomovil.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MenuHelpActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
     private View mDevice, mFaqs, mSupport;
+
+    RecyclerView list;
+    RecyclerView.LayoutManager layoutManager;
+    List<Item> items = new ArrayList<>();
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -25,19 +36,16 @@ public class MenuHelpActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_device:
-                    mTextMessage.setText(R.string.title_device);
                     mDevice.setVisibility(View.VISIBLE);
                     mFaqs.setVisibility(View.GONE);
                     mSupport.setVisibility(View.GONE);
                     return true;
                 case R.id.navigation_faqs:
-                    mTextMessage.setText(R.string.title_faqs);
                     mDevice.setVisibility(View.GONE);
                     mFaqs.setVisibility(View.VISIBLE);
                     mSupport.setVisibility(View.GONE);
                     return true;
                 case R.id.navigation_support:
-                    mTextMessage.setText(R.string.title_support);
                     mDevice.setVisibility(View.GONE);
                     mFaqs.setVisibility(View.GONE);
                     mSupport.setVisibility(View.VISIBLE);
@@ -61,9 +69,15 @@ public class MenuHelpActivity extends AppCompatActivity {
         mFaqs = findViewById(R.id.layout_faqs);
         mSupport = findViewById(R.id.layout_support);
 
-        mTextMessage = (TextView) findViewById(R.id.message_help);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation_help);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        list = (RecyclerView)findViewById(R.id.recycler);
+        list.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        list.setLayoutManager(layoutManager);
+
+        setData();
 
         Button btnSupport = (Button) findViewById(R.id.support_button);
         btnSupport.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +90,43 @@ public class MenuHelpActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void setData() {
+        Item item = new Item("¿Cómo califico una gasolinera?","Soy su hijo",true);
+        items.add(item);
+        Item item2 = new Item("No puedo conectar mi dispositivo","Soy su hijo",true);
+        items.add(item2);
+        Item item3 = new Item("¿Cómo localizar una gasolinera","Soy su hijo",true);
+        items.add(item3);
+        Item item4 = new Item("Actualizar datos de cuenta","Soy su hijo",true);
+        items.add(item4);
+        Item item5 = new Item("Autos compatibles con gasomovil","Soy su hijo",true);
+        items.add(item5);
+        Item item6 = new Item("No puedo inicar carga","Soy su hijo",true);
+        items.add(item6);
+        Item item7 = new Item("¿Cómo cambiar mi foto de perfil?","Soy su hijo",true);
+        items.add(item7);
+        Item item8 = new Item("Reporte de gasolinera","Soy su hijo",true);
+        items.add(item8);
+        Item item9 = new Item("¿Cómo comentar?","Soy su hijo",true);
+        items.add(item9);
+        /*for(int i=0;i<20;i++){
+            Item item = new Item("¿Cómo califico una gasolinera?","Soy su hijo",true);
+            item.setText("hola");
+            item.setSubText("nooo");
+            items.add(item);
+            if(i%2==0){
+                Item item = new Item("Soy Item"+(i+1),"Soy item hijo "+(i+1),true);
+                items.add(item);
+            }
+            else {
+                Item item = new Item("Soy Item"+(i+1),"",false);
+                items.add(item);
+            }
+        }*/
+        MyAdapter adapter = new MyAdapter(items);
+        list.setAdapter(adapter);
     }
 
     @Override
